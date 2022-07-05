d52fd88b89409359460d7fefa7fb7dda48ff415d
Ryan Clary <mrclary@gmail.com>
2022-07-04 19:31:01 -0700

    Add logger function

--- spy-install-subrepo.sh
+++ spy-install-subrepo.sh
@@ -11,12 +11,19 @@ Install subrepo in develop mode without dependencies
 EOF
 }
 
+exec 3>&1  # Additional output descriptor for logging
+log(){
+    level="INFO"
+    date "+%Y-%m-%d %H:%M:%S [$level] [install-subrepo] -> $1" 1>&3
+}
+
 SPYROOT=$(cd $(dirname $BASH_SOURCE)/../ 2> /dev/null && pwd -P)
 SPYREPO=$SPYROOT/spyder
 EXTDEPS=$SPYREPO/external-deps
 
 if [[ -z "$CONDA_DEFAULT_ENV" && -z "$PYENV_VERSION" && -z "$VIRTUAL_ENV" ]]; then
-    echo "Do not install subrepos into base environment. Activate an environment first."; exit 1
+    log "Do not install subrepos into base environment. Activate an environment first."
+    exit 1
 fi
 if [[ -e "$SPYREPO/install_dev_repos.py" ]]; then
     python -bb -X dev -W error $SPYREPO/install_dev_repos.py $@
--- spy-install-subrepos.sh
+++ spy-install-subrepos.sh
@@ -9,15 +9,23 @@ Install Spyder's subrepos in develop mode
 EOF
 }
 
+exec 3>&1  # Additional output descriptor for logging
+log(){
+    level="INFO"
+    date "+%Y-%m-%d %H:%M:%S [$level] [install-subrepos] -> $1" 1>&3
+}
+
 SPYROOT=$(cd $(dirname $BASH_SOURCE)/../ 2> /dev/null && pwd -P)
 SPYREPO=$SPYROOT/spyder
 EXTDEPS=$SPYREPO/external-deps
 
-echo "Installing subrepos..."
-
 if [[ -z "$CONDA_DEFAULT_ENV" && -z "$PYENV_VERSION" && -z "$VIRTUAL_ENV" ]]; then
-    echo "Do not install subrepos into base environment. Activate an environment first."; exit 1
+    log "Do not install subrepos into base environment. Activate an environment first."
+    exit 1
 fi
+
+log "Installing subrepos..."
+
 if [[ -e "$SPYREPO/install_dev_repos.py" ]]; then
     python -bb -X dev -W error $SPYREPO/install_dev_repos.py --no-install spyder
 else
