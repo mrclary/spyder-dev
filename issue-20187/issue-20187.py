c2c8464a4ff4f8133b1ec87f1eddac4da261ca96
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2023-03-04 15:58:02 -0800

    Issue 20187

---
+++ issue-20187/issue-20187.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+#plt.switch_backend('QT5Agg')
+plt.style.use("ggplot")
+
+t = np.arange(0.0, 2.0, 0.1)
+s = np.sin(2 * np.pi * t)
+s2 = np.cos(2 * np.pi * t)
+plt.plot(t, s, "o-", lw=4.1)
+plt.plot(t, s2, "o-", lw=4.1)
+plt.xlabel("time (s)")
+plt.ylabel("Voltage (mV)")
+plt.title("Simple plot $\\frac{\\alpha}{2}$")
+plt.grid(True)
+
+plt.savefig("test.png")
+
+plt.show()
