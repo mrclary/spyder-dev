90db00a42c83202bf13c37d996316f49f04903f5
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2023-04-13 12:29:00 -0700

    Do not prefix "spyder-" when supplying Spyder version Update use of menuinst to menuinst.api per deprecation warning Use menuinst.api to remove existing shortcut rather than rm

--- spy-menuinst.sh
+++ spy-menuinst.sh
@@ -2,8 +2,8 @@
 set -e
 
 ver=$1
-root_prefix=$HOME/Library/$ver
-prefix=$root_prefix/envs/$ver
+root_prefix=$HOME/Library/spyder-$ver
+prefix=$root_prefix/envs/spyder-$ver
 menu=$prefix/Menu/spyder-menu.json
 bundle=$HOME/Applications/Spyder.app
 
@@ -14,17 +14,26 @@ fi
 
 source $root_prefix/bin/activate base
 
-rm -rf $bundle
-python -c "import menuinst; menuinst.install('$menu', prefix='$prefix')"
+if [[ -e $bundle ]]; then
+    curr_ver=$(plutil -extract CFBundleShortVersionString raw "$bundle/Contents/Info.plist")
+    echo "Uninstalling Spyder.app version $curr_ver ..."
+    python -c "import menuinst; menuinst.api.remove('$menu')"
+fi
+
+echo "Installing Spyder.app version $ver ..."
+python -c "import menuinst; menuinst.api.install('$menu', target_prefix='$prefix')"
 
 if [[ ! -e $bundle ]]; then
     echo "Error: $bundle not created"
     exit 1
 fi
 
-echo "$bundle info:"
+echo "$bundle structure:"
 tree $bundle
+echo ""
+echo "$bundle/Contents/Info.plist contents:"
 cat $bundle/Contents/Info.plist
 echo ""
+echo "$bundle/Contents/MacOS/spyder-script contents:"
 cat $bundle/Contents/MacOS/spyder-script
 echo ""
