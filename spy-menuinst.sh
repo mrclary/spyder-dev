094b8a0b934169995678b739376b24016bcb4b79
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2023-06-13 09:35:11 -0700

    Accommodate Linux for shortcut creation

--- spy-menuinst.sh
+++ spy-menuinst.sh
@@ -5,7 +5,11 @@ ver=$1
 root_prefix=$HOME/Library/spyder-$ver
 prefix=$root_prefix/envs/spyder-$ver
 menu=$prefix/Menu/spyder-menu.json
-bundle=$HOME/Applications/Spyder.app
+if [[ $OSTYPE = "darwin"* ]]; then
+    shortcut=$HOME/Applications/Spyder.app
+else
+    shortcut=$HOME/.local/share/applicatons/spyder_spyder.desktop
+fi
 
 if [[ ! -e $menu ]]; then
     echo "Error: $menu not found"
@@ -14,26 +18,36 @@ fi
 
 source $root_prefix/bin/activate base
 
-if [[ -e $bundle ]]; then
-    curr_ver=$(plutil -extract CFBundleShortVersionString raw "$bundle/Contents/Info.plist")
-    echo "Uninstalling Spyder.app version $curr_ver ..."
+if [[ -e $shortcut && $OSTYPE = "darwin"* ]]; then
+    curr_ver=$(plutil -extract CFshortcutShortVersionString raw "$shortcut/Contents/Info.plist")
+    echo "Uninstalling Spyder.app bundle version ${curr_ver}..."
     python -c "import menuinst; menuinst.api.remove('$menu')"
 fi
 
-echo "Installing Spyder.app version $ver ..."
+if [[ $OSTYPE = "darwin"* ]]; then
+    echo "Installing Spyder.app bundle..."
+else
+    echo "Installing Spyder shortcut..."
+fi
 python -c "import menuinst; menuinst.api.install('$menu', target_prefix='$prefix')"
 
-if [[ ! -e $bundle ]]; then
-    echo "Error: $bundle not created"
+if [[ ! -e $shortcut ]]; then
+    echo "Error: $shortcut not created"
     exit 1
 fi
 
-echo "$bundle structure:"
-tree $bundle
-echo ""
-echo "$bundle/Contents/Info.plist contents:"
-cat $bundle/Contents/Info.plist
-echo ""
-echo "$bundle/Contents/MacOS/spyder-script contents:"
-cat $bundle/Contents/MacOS/spyder-script
-echo ""
+if [[ $OSTYPE = "darwin"* ]]; then
+    echo "$shortcut structure:"
+    tree $shortcut
+    echo ""
+    echo "$shortcut/Contents/Info.plist contents:"
+    cat $shortcut/Contents/Info.plist
+    echo ""
+    echo "$shortcut/Contents/MacOS/spyder-script contents:"
+    cat $shortcut/Contents/MacOS/spyder-script
+    echo ""
+else
+    echo "Contents of ${shortcut}:"
+    cat $shortcut
+    echo ""
+fi
