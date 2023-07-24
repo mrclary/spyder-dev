956de0ac016e8e9697af3f3d1a33300a3950f005
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2023-07-24 11:36:59 -0700

    issue 21173

---
+++ issue-21173/issue_21173.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 24 11:16:58 2023
+
+@author: rclary
+"""
+
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
