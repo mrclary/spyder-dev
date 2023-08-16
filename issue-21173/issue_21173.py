5db8b26fcdd719b75ff50b377cdd65b9d53370f7
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2023-08-16 13:48:32 -0700

    Update issue 21173

--- issue-21173/issue_21173.py
+++ issue-21173/issue_21173.py
@@ -14,3 +14,8 @@ class CreateCircle(Scene):
         circle = Circle()  # create a circle
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
+
+CreateCircle().construct()
+
+# from rich.console import Console
+# console = Console(color_system="truecolor", highlight=False)
