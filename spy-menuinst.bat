01657736578adfca8953cdc38199ab91ea1d683e
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-07-02 09:46:32 -0700

    spy-menuinst now only needs target env name. Base prefix is auto determined

--- spy-menuinst.bat
+++ spy-menuinst.bat
@@ -6,6 +6,7 @@ set UNINSTALL=true
 set INSTALL=true
 set bp=%localappdata%\spyder-6
 set tp=%bp%\envs\spyder-runtime
+set _bp=%localappdata%\miniforge3
 
 rem  Create variables from arguments
 :parse
@@ -13,23 +14,8 @@ if "%~1"=="" goto endparse
 if "%~1"=="-h" goto :help
 if "%~1"=="-u" set UNINSTALL=true& set INSTALL=false& shift
 if "%~1"=="-i" set INSTALL=true& set UNINSTALL=false& shift
-if "%~1"=="-b" set bp=%localappdata%\%~2& shift& shift
-if "%~1"=="-t" set tp=%userprofile%\.conda\envs\%~2& shift& shift
+if "%~1"=="-t" set bp=%_bp%& set tp=%userprofile%\.conda\envs\%~2& shift& shift
 goto parse
-:endparse
-
-set menu=%tp%\Menu\spyder-menu.json
-
-if "%UNINSTALL%"=="true" (
-    %bp%\python -c "from menuinst.api import remove; remove(r'%menu%', target_prefix=r'%tp%', base_prefix=r'%bp%')"
-)
-
-if "%INSTALL%"=="true" (
-    %bp%\python -c "from menuinst.api import install; install(r'%menu%', target_prefix=r'%tp%', base_prefix=r'%bp%')"
-)
-
-:exit
-    exit /b %errorlevel%
 
 :help
     @echo %~nx0 [options]
@@ -44,11 +30,27 @@ if "%INSTALL%"=="true" (
     @echo.
     @echo   -i         Install the shortcut
     @echo.
-    @echo   -b BASE    Base environment name, located in %localappdata%.
-    @echo              If not provided, %bp% is used
-    @echo              for the base prefix.
-    @echo.
     @echo   -t TARGET  Target environment name, located in %userprofile%\.conda\envs.
-    @echo              If not provided, %tp%
-    @echo              is used for the target prefix.
+    @echo              The base environment used is
+    @echo              %_bp%
+    @echo.
+    @echo              If not provided, the target prefix is
+    @echo              %tp%
+    @echo              and the base prefix is
+    @echo              %bp%
     goto :exit
+
+:endparse
+
+set menu=%tp%\Menu\spyder-menu.json
+
+if "%UNINSTALL%"=="true" (
+    %bp%\python -c "from menuinst.api import remove; remove(r'%menu%', target_prefix=r'%tp%', base_prefix=r'%bp%')"
+)
+
+if "%INSTALL%"=="true" (
+    %bp%\python -c "from menuinst.api import install; install(r'%menu%', target_prefix=r'%tp%', base_prefix=r'%bp%')"
+)
+
+:exit
+    exit /b %errorlevel%
--- spy-menuinst.sh
+++ spy-menuinst.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# Default is conda-based install location
+bp=$HOME/.local/spyder-6
+tp=$bp/envs/spyder-runtime
+[[ "$OSTYPE" == "darwin"* ]] && _bp=/usr/local/Caskroom/miniforge/base || _bp=$HOME/miniforge3
+
 help() { cat <<EOF
 
-$(basename $0) [options] VER
+$(basename $0) [options]
 
 Install and/or uninstall Spyder application launch shortcut.
 
@@ -16,7 +21,14 @@ Options:
   -i          Install the shortcut. If neither -u nor -i are specified,
               the shortcut will be uninstalled then installed.
 
-  VER         Base version to install/uninstall, e.g. 6
+  -t TARGET   Target environment name, located in $HOME/.conda/envs.
+              The base environment used is
+              $_bp
+
+              If not provided, the target prefix is
+              $tp
+              and the base prefix is
+              $bp
 
 EOF
 }
@@ -30,49 +42,43 @@ log(){
 UNINSTALL=0  # Default to uninstall
 INSTALL=0    # Default to install
 
-while getopts ":huiv:" option; do
+while getopts ":huit:n" option; do
     case $option in
         (h) help; exit ;;
         (u) UNINSTALL=0 && unset INSTALL ;;
         (i) unset UNINSTALL && INSTALL=0 ;;
+        (t) bp=$_bp; tp=$HOME/.conda/envs/$OPTARG ;;
+        (n) name=1 ;;
     esac
 done
 shift $(($OPTIND - 1))
-ver=$1
 
-if [[ -z "$ver" ]]; then
-    log "Please provide version."
-    help
-    exit 1
-fi
-
-if [[ "$OSTYPE" = "darwin"* ]]; then
-    root_prefix=$HOME/Library/spyder-$ver
-else
-    root_prefix=$HOME/.local/spyder-$ver
-fi
-[[ -f "${root_prefix}/.nonadmin" ]] && mode="user" || mode="system"
-prefix=$root_prefix/envs/spyder-runtime
-menu=$prefix/Menu/spyder-menu.json
+[[ -f "${tp}/.nonadmin" ]] && mode="user" || mode="system"
+menu=$tp/Menu/spyder-menu.json
 
 if [[ ! -e "$menu" ]]; then
     log "Error: $menu not found"
     exit 1
 fi
 
-source $root_prefix/bin/activate base
+source $bp/bin/activate base
 
 shortcut=$(python - <<EOF
 from menuinst.api import _load
-menu, menu_items = _load("$menu", target_prefix="$prefix", base_prefix="$root_prefix", _mode="$mode")
+menu, menu_items = _load("$menu", target_prefix="$tp", base_prefix="$bp", _mode="$mode")
 print(menu_items[0]._paths()[0])
 EOF
 )
 
+if [[ -n "$name" ]]; then
+    echo $shortcut
+    exit
+fi
+
 if [[ -n "$UNINSTALL" ]]; then
     if [[ -e "$shortcut" ]]; then
         log "Uninstalling ${shortcut} ..."
-        python -c "import menuinst; menuinst.api.remove('$menu', target_prefix='$prefix', base_prefix='$root_prefix')"
+        python -c "import menuinst; menuinst.api.remove('$menu', target_prefix='$tp', base_prefix='$bp')"
     else
         log "$shortcut already uninstalled."
     fi
@@ -82,7 +88,7 @@ fi
 
 if [[ -n "$INSTALL" ]]; then
     log "Installing $shortcut ..."
-    python -c "import menuinst; menuinst.api.install('$menu', base_prefix='$root_prefix', target_prefix='$prefix')"
+    python -c "import menuinst; menuinst.api.install('$menu', base_prefix='$bp', target_prefix='$tp')"
 
     if [[ ! -e "$shortcut" ]]; then
         log "Error: $shortcut not created"
