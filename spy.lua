dda2fcabdc7ff6798d70f61ae7e2842187b20d42
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-10-22 15:20:19 -0700

    Create module for lmod that loads spyder developer variables. Note: this replaces spy_var function in spy_dev.sh

---
+++ spy.lua
@@ -0,0 +1,28 @@
+help([[
+No help available
+
+]])
+whatis("Version: 0.1.0")
+whatis("Keywords: Spyder, Utility")
+whatis("Description: Spyder specific environment variables")
+
+family("context")
+depends_on_any("conda", "pyenv")
+
+-- Directory of this script
+spydev = myFileName():sub(1, myFileName():find(myModuleFullName()..".lua", 1, true)-2)
+spyide = splitFileName(spydev)
+spyrepo = pathJoin(spyide, 'spyder')
+
+setenv("SPYDEV", spydev)
+setenv("SPYREPO", spyrepo)
+set_alias("spy-build-installers", pathJoin(spydev, "spy-build-installers.sh"))
+set_alias("spy-clone-subrepo", pathJoin(spydev, "spy-clone-subrepo.sh"))
+set_alias("spy-env", pathJoin(spydev, "spy-env.sh"))
+set_alias("spy-menuinst", pathJoin(spydev, "spy-menuinst.sh"))
+set_alias("spy-update-conda-app", pathJoin(spydev, "spy-update-conda-app.sh"))
+set_alias("spy-dev-spyder", "mamba run --live-stream -n spy-dev python "..pathJoin(spyrepo, "bootstrap.py"))
+
+if io.popen("echo $OSTYPE"):read():find("darwin") then
+  set_alias("spy-app-spyder", pathJoin(os.getenv("HOME"), "/Applications/Spyder\\ 6.app/Contents/MacOS/spyder-6"))
+end
