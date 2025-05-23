55782793e9d2bef2b4feb558669c1a3c9f248ef9
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2025-05-23 08:51:32 -0700

    Add docstring and default version

--- spy-update-conda-app.sh
+++ spy-update-conda-app.sh
@@ -1,8 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
-ver=$1
-shift
+script_path=${BASH_SOURCE:-${(%):-%x}}
+here=$(dirname $script_path)
+spyrepo=$(dirname $here)/spyder
+
+ver=6
+
+help(){ cat <<EOF
+$(basename $script_path) [options]
+
+Install local Spyder repository and friends ($spyrepo)
+in editable mode in the installer location.
+Requires that Spyder is already installed via installer.
+
+Options:
+  -h      Display this help and exit.
+  -v VER  Spyder major version (default $ver).
+
+Additional options are passed to $spyrepo/install_dev_repos.py
+
+------------------------------------------------------------------------------
+
+$(conda run python $spyrepo/install_dev_repos.py --help)
+
+EOF
+}
+
+while getopts ":hv:" option; do
+    case $option in
+        (h) help; exit ;;
+        (v) ver=$OPTARG
+    esac
+done
+shift $(($OPTIND - 1))
 
 if [[ "$OSTYPE" = "darwin"* ]]; then
     ROOT_PREFIX=$HOME/Library/spyder-${ver}
@@ -11,4 +42,4 @@ else
 fi
 PREFIX=${ROOT_PREFIX}/envs/spyder-runtime
 
-conda run --live-stream -p $PREFIX python $SPYREPO/install_dev_repos.py "$@"
+conda run --live-stream -p $PREFIX python $spyrepo/install_dev_repos.py "$@"
